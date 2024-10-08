fab69030b0923bcfd47fc45883ae7366fe154cf7
S M ASHADUZZAMAN <137910960+Ashaduzzaman12@users.noreply.github.com>
2024-10-08 07:30:06 +0600

    Create cycle_detection_dfs.cpp

---
+++ cycle_detection_dfs.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std; 
+const int N=1e5+5;
+bool vis[N];
+vector<int> adj[N];
+int parentarr[N];
+bool ans;
+void dfs(int parent){
+    vis[parent]=true;
+    for(int child:adj[parent]){
+        if(vis[child]&&child!= parentarr[parent]){
+            ans=true;
+        }
+        if(!vis[child]){
+            dfs(child);
+            parentarr[child]=parent;
+        }
+    }
+}
+int main() {
+    int n,e;
+    cin>>n>>e;
+    while (e--)
+    {
+        int a ,b;
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    memset(vis,false ,sizeof(vis));
+    memset(parentarr,-1,sizeof(parentarr));
+    ans=false;
+    for (int i = 0; i < n; i++)
+    {
+        if(!vis[i]){
+            dfs(i);
+        }
+    }
+    if(ans){
+        cout<<"cycle found ";
+    }
+    else{
+        cout<<"cycle not found";
+    }
+    return 0;
+}
